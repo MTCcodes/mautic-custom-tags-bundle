e67241defc4015bb020047e7fb646305f412ae10
Leo Borlot <leonardoborlot@gmail.com>
2024-09-24 14:07:00 -0300

    Adicionei os metodos de encrypt e decrypt

--- Helper/TokenHelper.php
+++ Helper/TokenHelper.php
@@ -12,7 +12,6 @@
 namespace MauticPlugin\MauticCustomTagsBundle\Helper;
 
 use GuzzleHttp\Client;
-use Joomla\Http\Http;
 use Mautic\LeadBundle\Entity\Lead;
 use Mautic\LeadBundle\Helper\PrimaryCompanyHelper;
 
@@ -22,7 +21,7 @@ use Mautic\LeadBundle\Helper\PrimaryCompanyHelper;
 class TokenHelper
 {
     /**
-     * @var Client ;
+     * @var Client
      */
     protected $connector;
 
@@ -32,7 +31,7 @@ class TokenHelper
     private $primaryCompanyHelper;
 
     /**
-     * EmailSubscriber constructor.
+     * TokenHelper constructor.
      */
     public function __construct(Client $connector, PrimaryCompanyHelper $primaryCompanyHelper)
     {
@@ -50,10 +49,12 @@ class TokenHelper
     {
         $tokens = [];
 
-        // convert Lead entity to array
+        // Convert Lead entity to array
         if ($lead instanceof Lead) {
             $lead = $this->primaryCompanyHelper->getProfileFieldsWithPrimaryCompany($lead);
         }
+
+        // Process {getremoteurl=...} tokens
         preg_match_all('/{getremoteurl=(.*?)}/', $content, $matches);
         if (count($matches[0])) {
             foreach ($matches[1] as $k => $url) {
@@ -63,32 +64,115 @@ class TokenHelper
                     continue;
                 }
                 try {
-                    $urlWithoutDecode             = str_replace(['|decode', '%7Cdecode'], '', $url);
-                    $isDecodeToken                = $urlWithoutDecode !== $url;
-                    $url                          = $isDecodeToken ? htmlspecialchars_decode(urldecode($urlWithoutDecode)) : $url;
-                    $url                          = \Mautic\LeadBundle\Helper\TokenHelper::findLeadTokens($url = str_replace(['[', ']'], ['{', '}'], $url), $lead, true);
-                    $data                         = $this->connector->get(
-                        $url,
-                        []
+                    $urlWithoutDecode = str_replace(['|decode', '%7Cdecode'], '', $url);
+                    $isDecodeToken    = $urlWithoutDecode !== $url;
+                    $url              = $isDecodeToken ? htmlspecialchars_decode(urldecode($urlWithoutDecode)) : $url;
+                    $url              = \Mautic\LeadBundle\Helper\TokenHelper::findLeadTokens(
+                        str_replace(['[', ']'], ['{', '}'], $url),
+                        $lead,
+                        true
                     );
+                    $data             = $this->connector->get($url, []);
                     $tokens[$token]   = $data->getBody()->getContents();
                 } catch (\Exception $e) {
                     $tokens[$token] = '';
                 }
             }
         }
+
+        // Process {base64decode=...} tokens
         preg_match_all('/{base64decode=(.*?)}/', $content, $matches);
         if (count($matches[0])) {
-            foreach ($matches[1] as $k => $url) {
+            foreach ($matches[1] as $k => $field) {
                 $token = $matches[0][$k];
 
                 if (isset($tokens[$token])) {
                     continue;
                 }
-                $tokens[$token] =  (!empty($lead[$url])) ? base64_decode($lead[$url]) : '';
+                $tokens[$token] = (!empty($lead[$field])) ? base64_decode($lead[$field]) : '';
+            }
+        }
+
+        // Process {encrypt=...} tokens
+        preg_match_all('/{encrypt=(.*?)}/', $content, $matches);
+        if (count($matches[0])) {
+            foreach ($matches[1] as $k => $field) {
+                $token = $matches[0][$k];
+
+                if (isset($tokens[$token])) {
+                    continue;
+                }
+                $tokens[$token] = (!empty($lead[$field])) ? $this->encrypt($lead[$field]) : '';
+            }
+        }
+
+        // Process {decrypt=...} tokens
+        preg_match_all('/{decrypt=(.*?)}/', $content, $matches);
+        if (count($matches[0])) {
+            foreach ($matches[1] as $k => $encryptedData) {
+                $token = $matches[0][$k];
+
+                if (isset($tokens[$token])) {
+                    continue;
+                }
+                $tokens[$token] = $this->decrypt($encryptedData);
             }
         }
 
         return str_replace(array_keys($tokens), $tokens, $content);
+    }
+
+    /**
+     * Encrypt data using passphrase.
+     *
+     * @param string $data
+     *
+     * @return string
+     */
+    public function encrypt($data)
+    {
+        $passphrase = $this->getPassphrase();
+        $method     = 'AES-256-CBC';
+        $key        = hash('sha256', $passphrase, true);
+        $iv         = openssl_random_pseudo_bytes(16);
+        $encrypted  = openssl_encrypt($data, $method, $key, OPENSSL_RAW_DATA, $iv);
+
+        // Return the IV and encrypted data encoded in base64
+        return base64_encode($iv . $encrypted);
+    }
+
+    /**
+     * Decrypt data using passphrase.
+     *
+     * @param string $data
+     *
+     * @return string
+     */
+    public function decrypt($data)
+    {
+        $passphrase = $this->getPassphrase();
+        $method     = 'AES-256-CBC';
+        $key        = hash('sha256', $passphrase, true);
+        $data       = base64_decode($data);
+        $iv         = substr($data, 0, 16);
+        $encrypted  = substr($data, 16);
+        $decrypted  = openssl_decrypt($encrypted, $method, $key, OPENSSL_RAW_DATA, $iv);
+
+        return $decrypted;
+    }
+
+    /**
+     * Get the passphrase from ENV or use a default value.
+     *
+     * @return string
+     */
+    private function getPassphrase()
+    {
+        $passphrase = getenv('ENCRYPTION_PASSPHRASE');
+        if (!$passphrase) {
+            $passphrase = 'default_passphrase'; // Use your default passphrase here
+        }
+
+        return $passphrase;
     }
 }
